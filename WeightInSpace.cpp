bbd8f1c85fbb217d56d8d9c74e51179e327415e5
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-12 10:53:42 +0100

    Update weight program
    Add more conditionals into weight program to account for all planets
    in the solar system and put them into a switch statement.

---
+++ WeightInSpace.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double earthWeight;
+  int planet;
+  std::cout << "Hello, what is your Earth weight in kg?\n";
+  std::cin >> earthWeight;
+  std::cout << "What planet would you like to fight on? Please select a number between 1 and 7.\n";
+  std::cin >> planet;
+  switch (planet)
+  {
+  case 1:
+    std::cout << "Your weight on Mercury is " << earthWeight * 0.38 << "\n";
+    break;
+  case 2:
+    std::cout << "Your weight on Venus is " << earthWeight * 0.91 << "\n";
+    break;
+  case 3:
+    std::cout << "Your weight on Mars is " << earthWeight * 0.38 << "\n";
+    break;
+  case 4:
+    std::cout << "Your weight on Jupiter is " << earthWeight * 2.34 << "\n";
+    break;
+  case 5:
+    std::cout << "Your weight on Saturn is " << earthWeight * 1.06 << "\n";
+    break;
+  case 6:
+    std::cout << "Your weight on Uranus is " << earthWeight * 0.92 << "\n";
+    break;
+  case 7:
+    std::cout << "Your weight on Neptune is " << earthWeight * 1.19 << "\n";
+    break;
+  default:
+    std::cout << "I'm sorry, you have entered an invalid planet\n";
+    break;
+  }
+}
