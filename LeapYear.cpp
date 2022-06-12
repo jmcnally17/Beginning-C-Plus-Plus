e4a2736280c13752f3c40f8a30904568b2602d54
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-12 11:07:32 +0100

    Create leap year program
    Take an integer from input from the user and determine whether or not
    that numnber year is a leap year.

---
+++ LeapYear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+  int year;
+  std::cout << "Please enter a year:\n";
+  std::cin >> year;
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+  {
+    std::cout << year << " is a leap year!\n";
+  }
+  else
+  {
+    std::cout << year << " is not a leap year.\n";
+  }
+}
