d9d9ca30150b47d43a34ba6089be71d99bb978dd
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-28 00:17:59 +0100

    Change cout messages
    Separate the two integer inputs into separate cout messages so that each
    one can be validated individually.

--- Calculation/Calculation.cpp
+++ Calculation/Calculation.cpp
@@ -4,9 +4,10 @@ int main()
 {
   int num1;
   int num2;
-  std::cout << "Hello, welcome to our simple calculator!\n";
-  std::cout << "Please enter two integers to be multiplied:\n";
+  std::cout << "Hello, welcome to our simple multiplication calculator!\n";
+  std::cout << "Please enter your first integer to be multiplied:\n";
   std::cin >> num1;
+  std::cout << "Please enter your second integer to be multiplied:\n";
   std::cin >> num2;
   std::cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
 }
