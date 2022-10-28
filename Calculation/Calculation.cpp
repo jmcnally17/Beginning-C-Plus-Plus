61451b907199ad3ffc5534f9b9631bfbd7707f00
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-28 01:03:39 +0100

    Add cin validator for first integer
    Using the streamsize along with cin.clear() and cin.ignore(), add in
    a validator for the first input from the user to check that an integer is
    inputted.

--- Calculation/Calculation.cpp
+++ Calculation/Calculation.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <ios>
+#include <limits>
+using namespace std;
 
 int main()
 {
   int num1;
   int num2;
-  std::cout << "Hello, welcome to our simple multiplication calculator!\n";
-  std::cout << "Please enter your first integer to be multiplied:\n";
-  std::cin >> num1;
-  std::cout << "Please enter your second integer to be multiplied:\n";
-  std::cin >> num2;
-  std::cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
+  cout << "Hello, welcome to our simple multiplication calculator!\n";
+  while (true)
+  {
+    cout << "Please enter your first integer to be multiplied:\n";
+    cin >> num1;
+    if (cin.fail())
+    {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "You did not enter an integer!\n";
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  cout << "Please enter your second integer to be multiplied:\n";
+  cin >> num2;
+  cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
 }
