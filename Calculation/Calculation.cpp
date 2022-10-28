457fcbd411ba31d7ac9f06d2c307251255d2c7c9
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-28 01:08:01 +0100

    Add validator for second integer
    Create a validator for the second integer inputted by the user using the
    same method as the first one.

--- Calculation/Calculation.cpp
+++ Calculation/Calculation.cpp
@@ -8,6 +8,7 @@ int main()
   int num1;
   int num2;
   cout << "Hello, welcome to our simple multiplication calculator!\n";
+
   while (true)
   {
     cout << "Please enter your first integer to be multiplied:\n";
@@ -24,7 +25,21 @@ int main()
     }
   }
 
-  cout << "Please enter your second integer to be multiplied:\n";
-  cin >> num2;
+  while (true)
+  {
+    cout << "Please enter your second integer to be multiplied:\n";
+    cin >> num2;
+    if (cin.fail())
+    {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "You did not enter an integer!\n";
+    }
+    else
+    {
+      break;
+    }
+  }
+
   cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
 }
