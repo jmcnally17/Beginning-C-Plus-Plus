71c8550324ee1f41fe6661c64a498a883d7bf50b
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-28 02:51:42 +0100

    Reformat validators
    Edit the validators to make the code more readable.

--- Calculation/Calculation.cpp
+++ Calculation/Calculation.cpp
@@ -1,45 +1,33 @@
 #include <iostream>
-#include <ios>
 #include <limits>
-using namespace std;
 
 int main()
 {
   int num1;
   int num2;
-  cout << "Hello, welcome to our simple multiplication calculator!\n";
+  std::cout << "Hello, welcome to our simple multiplication calculator!\n";
 
-  while (true)
+  std::cout << "Please enter your first integer to be multiplied:\n";
+  std::cin >> num1;
+  while (std::cin.fail())
   {
-    cout << "Please enter your first integer to be multiplied:\n";
-    cin >> num1;
-    if (cin.fail())
-    {
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cout << "You did not enter an integer!\n";
-    }
-    else
-    {
-      break;
-    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "You did not enter an integer!\n";
+    std::cout << "Please enter your first integer to be multiplied:\n";
+    std::cin >> num1;
   }
 
-  while (true)
+  std::cout << "Please enter your second integer to be multiplied:\n";
+  std::cin >> num2;
+  while (std::cin.fail())
   {
-    cout << "Please enter your second integer to be multiplied:\n";
-    cin >> num2;
-    if (cin.fail())
-    {
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cout << "You did not enter an integer!\n";
-    }
-    else
-    {
-      break;
-    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "You did not enter an integer!\n";
+    std::cout << "Please enter your second integer to be multiplied:\n";
+    std::cin >> num2;
   }
 
-  cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
+  std::cout << num1 << " multiplied by " << num2 << " equals " << num1 * num2 << "!\n";
 }
