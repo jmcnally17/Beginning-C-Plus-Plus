45cafde0d6062f5a8731893652132f918862bdf1
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-11-09 13:19:52 +0000

    Replace int with double
    Change the type of num1 and num2 to a double in order to properly multiply
    decimals.

--- Calculation/Calculation.cpp
+++ Calculation/Calculation.cpp
@@ -3,29 +3,29 @@
 
 int main()
 {
-  int num1;
-  int num2;
+  double num1;
+  double num2;
   std::cout << "Hello, welcome to our simple multiplication calculator!\n";
 
-  std::cout << "Please enter your first integer to be multiplied:\n";
+  std::cout << "Please enter your first number to be multiplied:\n";
   std::cin >> num1;
   while (std::cin.fail())
   {
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "You did not enter an integer!\n";
-    std::cout << "Please enter your first integer to be multiplied:\n";
+    std::cout << "You did not enter a number!\n";
+    std::cout << "Please enter your first number to be multiplied:\n";
     std::cin >> num1;
   }
 
-  std::cout << "Please enter your second integer to be multiplied:\n";
+  std::cout << "Please enter your second number to be multiplied:\n";
   std::cin >> num2;
   while (std::cin.fail())
   {
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "You did not enter an integer!\n";
-    std::cout << "Please enter your second integer to be multiplied:\n";
+    std::cout << "You did not enter a number!\n";
+    std::cout << "Please enter your second number to be multiplied:\n";
     std::cin >> num2;
   }
 
